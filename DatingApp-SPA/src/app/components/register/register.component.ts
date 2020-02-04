a537da5288be0119db090cd7eebd6b69243c5d0e
ctoxy <cedric.bouve@gmail.com>
2020-02-04 11:48:39 +0100

    remove unused data @input

--- DatingApp-SPA/src/app/components/register/register.component.ts
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { AuthService } from 'src/app/_services/auth.service';
 
 
@@ -8,7 +8,7 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
+
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
