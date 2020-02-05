ddcb78bdff864af882099e288933ac089446cb62
ctoxy <cedric.bouve@gmail.com>
2020-02-05 14:55:21 +0100

    conserver nom user

--- DatingApp-SPA/src/app/components/nav/nav.component.ts
+++ DatingApp-SPA/src/app/components/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
