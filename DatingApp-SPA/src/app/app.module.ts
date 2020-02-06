c2004126d5494dd4dd4d1432685e8c3145972945
ctoxy <cedric.bouve@gmail.com>
2020-02-06 15:56:58 +0100

    using jwttoken in user services

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,20 +3,30 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
+import { JwtModule } from '@auth0/angular-jwt';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
+
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
+
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { MemberCardComponent } from './components/members/member-card/member-card.component';
 
+
+/* evite lors de la connexion que le token soit null pour la requete vers le serveru */
+export function tokenGetter() {
+  return localStorage.getItem('token')
+}
 
 @NgModule({
   declarations: [
@@ -38,7 +48,14 @@ import { MemberCardComponent } from './components/members/member-card/member-car
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
