be2726991327339b7caecf7bd7caf8693f188b5b
ctoxy <cedric.bouve@gmail.com>
2020-02-12 10:59:24 +0100

    adding pagination in front

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -10,6 +10,8 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
   constructor(
     private userService: UserService,
     private router: Router,
@@ -17,7 +19,7 @@ export class MemberListResolver implements Resolve<User[]> {
   ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers().pipe(
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
       catchError(error => {
         this.alertify.error('Problem retrieving data members');
         this.router.navigate(['/home']);
