906626568a4dc0e31ed39448cf40c817b5d73d50
ctoxy <cedric.bouve@gmail.com>
2020-02-12 10:32:33 +0100

    setting in the response back pagination in get user

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace DatingApp.API.Controllers
 {
     [ServiceFilter(typeof(LogUserActivity))]
@@ -29,10 +30,12 @@ namespace DatingApp.API.Controllers
         // GET api/users
         
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var users = await _repo.GetUsers(userParams);
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+            Response.AddPagination(users.CurrentPage,
+                users.PageSize, users.TotalCount, users.TotalPages);
             return Ok(usersToReturn);
         }
 
