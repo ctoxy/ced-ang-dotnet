63b1e3ead73270056930415677c4210555405c28
ctoxy <cedric.bouve@gmail.com>
2020-02-13 14:25:53 +0100

    Adding the Mark as Read functionality

--- DatingApp-SPA/src/app/components/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/components/members/member-messages/member-messages.component.ts
@@ -25,10 +25,12 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    /* +  pour avoir une correspondance de number */
     const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
       .pipe(
         tap(messages => {
+          // tslint:disable-next-line: prefer-for-of
           for (let i = 0; i < messages.length; i++) {
             if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
               this.userService.markAsRead(currentUserId, messages[i].id);
