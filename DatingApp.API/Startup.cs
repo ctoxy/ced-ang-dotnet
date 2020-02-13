4626040217aabf204fc3ce94fd6cb78c414f5033
ctoxy <cedric.bouve@gmail.com>
2020-02-13 16:29:06 +0100

    Adding additional Database providers

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -23,6 +23,21 @@ namespace DatingApp.API
             Configuration = configuration;
         }
 
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseSqlite
+            (Configuration.GetConnectionString("DefaultConnection")));
+
+            ConfigureServices(services);
+        }
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseMySql
+            (Configuration.GetConnectionString("DefaultConnection")));
+
+            ConfigureServices(services);
+        }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
