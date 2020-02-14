e25ceaedd0301d6598e76c1f605efeefdd0afd13
ctoxy <cedric.bouve@gmail.com>
2020-02-14 10:38:35 +0100

    Configuring the DataContext for Identity

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,13 +1,17 @@
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User, Role, int,
+        IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>,
+        IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
+        
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
@@ -16,7 +20,25 @@ namespace DatingApp.API.Data
         info pour entity lors de la creation de la relation user et message many to many
         k.MessageSent, k.MessageReceived pour evieter d'aimer plusieurs fois on concatene les deux clé primaire*/
         protected override void OnModelCreating(ModelBuilder builder)
-        {
+        {   
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole => 
+            {
+                /* relation many to many un user peux avoir plusieur role et un role plusieur user */
+                userRole.HasKey(ur => new {ur.UserId, ur.RoleId} );
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+                    
+
+            });
             builder.Entity<Like>()
                 .HasKey(k => new {k.LikerId, k.LikeeId});
             builder.Entity<Like>()
