f81272cc36106e34afc682b4c82dca02548b89b4
ctoxy <cedric.bouve@gmail.com>
2020-02-14 11:58:27 +0100

    Updating the Seed Users method

--- DatingApp.API/Data/seed.cs
+++ DatingApp.API/Data/seed.cs
@@ -1,30 +1,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
 
 namespace DatingApp.API.Data
 {
     public class seed
     {
-        public static void SeedUsers(DataContext context)
+        public static void SeedUsers(UserManager<User> userManager)
         {
-            if (!context.Users.Any())
+            if (!userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
                 foreach (var user in users)
                 {
-                    byte[] passwordhash, passwordSalt;
-                    CreatePasswordHash("password", out passwordhash, out passwordSalt);
-
-                    /*user.PasswordHash = passwordhash;
-                    user.PasswordSalt = passwordSalt;*/
-                    user.UserName = user.UserName.ToLower();
-                    context.Users.Add(user);
+                    userManager.CreateAsync(user, "password").Wait();
+                    
                 }
 
-                context.SaveChanges();
+                
             }
         }
         private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
