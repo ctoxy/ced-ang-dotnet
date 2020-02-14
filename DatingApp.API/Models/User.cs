07a6043f4bd721558c3976d3f0b567330d4a3583
ctoxy <cedric.bouve@gmail.com>
2020-02-14 10:08:21 +0100

    Adding the Identity models

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace DatingApp.API.Models
 {
-    public class User
+    // using identity nom commun donc suppriession specifier int car par defaut string dans identity
+    public class User :IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public string Gender { get; set; }
 
         public DateTime DateOfBirth { get; set; }
@@ -25,5 +23,6 @@ namespace DatingApp.API.Models
         public ICollection<Like> Likees { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
     }
 }
